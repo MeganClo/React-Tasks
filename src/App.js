b8d028fe25378dc4536394969ea94e35db4cbf5c
Megs <Megs.close@gmail.com>
2021-06-04 13:48:07 -0700

    header testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header"
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
-      <h2>Testing React!</h2>
+    <div className="container">
+      <Header/>
     </div>
 
   );
