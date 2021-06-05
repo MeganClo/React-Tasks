ec1487238ef9312a985716e86f5653eaa5acf401
Megs <Megs.close@gmail.com>
2021-06-05 13:56:12 -0700

    using state

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,26 @@
-import { FaTimes } from "react-icons/fa"
+import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className="task">
-            <h3>{task.text} <FaTimes style={{ color: "red", cursor: "pointer" }} onClick={() => onDelete(task.id)}/></h3>
-            <p>{task.day}</p>
+        <div style={{"display": "flex", "justifyColumn": "center", "alignItems": "center"}}>
+            <div
+                className={`task ${!task.reminder && 'reminder'}`}
+                onClick={() => onToggle(task.id)}
+                style={{width: "90%"}}
+                
+
+            >
+                <h3>
+                    {task.text}{' '}
+
+                </h3>
+                <p>{task.day}</p>
+            </div>
+            <div style={{width: "10%"}}> 
+                <FaTimes
+                style={{ color: 'red', cursor: 'pointer' }}
+                onClick={() => onDelete(task.id)}
+            /></div>
         </div>
     )
 }
